536cd385a57d5265af65b157e61d569ba3dd4dfa
Cheekibreaki <jiping.li@mail.utoronto.ca>
2023-11-22 14:20:01 -0500

    ready for openend

--- simplesim-3.0d-assig4/runcmd.sh
+++ simplesim-3.0d-assig4/runcmd.sh
@@ -1,9 +1,9 @@
 # make clean
 make sim-cache
 
-./sim-cache -redir:sim compress_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/compress.eio 
-./sim-cache -redir:sim gcc_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/gcc.eio 
-./sim-cache -redir:sim go_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/go.eio 
+# ./sim-cache -redir:sim compress_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/compress.eio 
+# ./sim-cache -redir:sim gcc_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/gcc.eio 
+# ./sim-cache -redir:sim go_nextline.simout -config cache-config/cache-lru-nextline.cfg /cad2/ece552f/benchmarks/go.eio 
 
 ./sim-cache -redir:sim compress_stride.simout -config cache-config/cache-lru-stride.cfg /cad2/ece552f/benchmarks/compress.eio 
 ./sim-cache -redir:sim gcc_stride.simout -config cache-config/cache-lru-stride.cfg /cad2/ece552f/benchmarks/gcc.eio

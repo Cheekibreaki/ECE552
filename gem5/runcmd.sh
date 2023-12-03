fcfb774f90a2a9e9208528107c3d11d1ebd2ed1e
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-12-03 00:46:50 -0500

    save cache changes

--- gem5/runcmd.sh
+++ gem5/runcmd.sh
@@ -1,4 +1,4 @@
-# scons PROTOCOL=MSI CPU_MODELS=TimingSimpleCPU build/ALPHA/gem5.opt
+scons PROTOCOL=MSI CPU_MODELS=TimingSimpleCPU build/ALPHA/gem5.opt
 
 # ./build/ALPHA/gem5.opt ./configs/example/ruby_random_test.py
-./build/ALPHA/gem5.opt --debug-flags=ProtocolTrace ./configs/example/ruby_random_test.py -n 3
+./build/ALPHA/gem5.opt --debug-flags=ProtocolTrace ./configs/example/ruby_random_test.py -n 4
